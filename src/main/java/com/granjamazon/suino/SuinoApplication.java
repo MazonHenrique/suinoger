7639de4f93ded888b69f0c3084647a5abe28f352
MazonHenrique <hmazon87@yahoo.com>
2023-06-20 21:17:16 -0300

    criacao do projeto

---
+++ src/main/java/com/granjamazon/suino/SuinoApplication.java
@@ -0,0 +1,13 @@
+package com.granjamazon.suino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuinoApplication.class, args);
+	}
+
+}
